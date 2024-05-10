b52cd51f762ab0d92d9b839a1e96599a33ba91a9
somikaaa <124293047+somikaaa@users.noreply.github.com>
2024-05-11 02:23:51 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,86 +4,86 @@
 
 //тест на включение автомата
 TEST(AutomataTest, IsOnTest) {
-	Automata automata;
-	automata.on();
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 //тест на выключение автомата
 TEST(AutomataTest, IsOffTest){
-	Automata automata;
-	automata.on();
-	automata.off();
-	EXPECT_EQ(automata.getState(), STATES::OFF);
+     Automata automata;
+     automata.on();
+     automata.off();
+     EXPECT_EQ(automata.getState(), STATES::OFF);
 }
 
 //тест на чтение меню
 TEST(AutomataTest, ReadMenuTest) {
-	Automata automata;
-	automata.on();
-	automata.getMenu();
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.getMenu();
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 //тест на внесение денег
 TEST(AutomataTest, CoinsTest) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	EXPECT_EQ(automata.getState(), STATES::ACCEPT);
+     Automata automata;
+     automata.on();
+     automata.coin(50);
+     EXPECT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
 //тест на отмену покупки и возврат денег
 TEST(AutomataTest, CancelTest) {
-	Automata automata;
-	automata.on();
-	automata.coin(10);
-	automata.cancel();
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.coin(10);
+     automata.cancel();
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 // тест на имитацию приготовления кофе(1)
 TEST(AutomataTest, CookTest1) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(0);
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.coin(50);
+     automata.choice(0);
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 // тест на имитацию приготовления чая(2)
 TEST(AutomataTest, CookTest2) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(1);
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.coin(50);
+     automata.choice(1);
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 // тест на имитацию приготовления горячего шоколада(6)
 TEST(AutomataTest, CookTest3) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(5);
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.coin(50);
+     automata.choice(5);
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
 //тест на имитацию отмены обслуживания и повторного внесения денег
 TEST(AutomataTest, CanselAndCoinTest) {
-	Automata automata;
-	automata.on();
-	automata.coin(100);
-	automata.cancel();
-	automata.coin(50);
-	EXPECT_EQ(automata.getState(), STATES::ACCEPT);
+     Automata automata;
+     automata.on();
+     automata.coin(100);
+     automata.cancel();
+     automata.coin(50);
+     EXPECT_EQ(automata.getState(), STATES::ACCEPT);
 }
 
 // тест на имитацию завершения приотовления напитка
 TEST(AutomataTest, FinishTest) {
-	Automata automata;
-	automata.on();
-	automata.finish();
-	EXPECT_EQ(automata.getState(), STATES::WAIT);
+     Automata automata;
+     automata.on();
+     automata.finish();
+     EXPECT_EQ(automata.getState(), STATES::WAIT);
 }
 
